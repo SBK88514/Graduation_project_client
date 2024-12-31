727bf32030f8f5eaf46a4470bad805603b698ca6
Chaim-Pollak <ch324id@gmail.com>
2025-01-01 01:58:09 +0200

    added logout function

--- src/components/contexts/ActionContext.jsx
+++ src/components/contexts/ActionContext.jsx
@@ -3,37 +3,34 @@ import { createContext, useState } from "react";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-
 export const ActionContext = createContext();
 
 function ActionProvider({ children }) {
-
   const quaryClient = useQueryClient();
   const { mutate: mutateUpdate } = useMutation({
     mutationKey: ["update_issue"],
     mutationFn: async (idEmpIss) => axios.put("/issues/updateissue", idEmpIss),
     onSuccess: () => {
       quaryClient.invalidateQueries({ queryKey: ["get_issues"] });
-      console.log(1)
     },
     onError: () => {},
   });
-  const [issues, setIssues] = useState(null)
-  const{mutate: mutateMyIssue} = useMutation({
-      mutationKey: ["get_my_issues"],
-      mutationFn: async (idEmployee) =>  axios.post("/users/getemployeebyid",idEmployee),   
-      onSuccess: (data) => {
-        setIssues(data.data.data)
-        console.log(data)
-        },
-      onError: () =>{}
-      
-  })
+  const [issues, setIssues] = useState(null);
+  const { mutate: mutateMyIssue } = useMutation({
+    mutationKey: ["get_my_issues"],
+    mutationFn: async (idEmployee) =>
+      axios.post("/users/getemployeebyid", idEmployee),
+    onSuccess: (data) => {
+      setIssues(data.data.data);
+      console.log(data);
+    },
+    onError: () => {},
+  });
 
   const value = {
     mutateUpdate,
     mutateMyIssue,
-    issues
+    issues,
   };
 
   return (
